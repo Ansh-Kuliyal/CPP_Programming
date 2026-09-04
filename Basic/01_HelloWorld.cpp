95fd6e71d1fc6a7fda93351eb57f643cb0ec1ef1
Ansh Kuliyal <kuliyalansh89@gmail.com>
2026-09-04 14:52:08 +0530

    Learned to Print Hello World in C++

---
+++ Basic/01_HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main()
+{
+    // This is comment
+    /* 
+       This
+       is
+       a
+       multi-line
+       comment
+    */
+
+    std::cout<<"Hello World"<<std::endl;   // can also use '\n'; instead of "std::endl;"
+    std::cout<<"This is best"<<std::endl;
+    return 0;
+}
