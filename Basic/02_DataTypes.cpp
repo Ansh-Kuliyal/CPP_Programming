fa2c31ce73f59cd29377f86a7266ff4cfc038833
Ansh Kuliyal <kuliyalansh89@gmail.com>
2026-09-04 14:58:15 +0530

    Learned Variables and Data Types in C++

---
+++ Basic/02_DataTypes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    // integer (whole number)
+    int age = 21;
+    int year = 18;
+
+    // double (number including decimal)
+    double price = 10.99;
+    double gpa = 5.6;
+
+    // single character
+    char grade = 'A';
+    char currency = '$';
+
+    // boolean (true or false)
+    bool student = true;
+    bool isOnline = false;
+
+    // string (objects that represents a sequence of text)
+    std::string name = "Void";
+    std::string food = "Pizza";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << year << " years old" << '\n';
+
+    return 0;
+}
