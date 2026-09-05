4a61ad947e83cd0a51bf5c4eb4498c4238b6475e
Ansh Kuliyal <kuliyalansh89@gmail.com>
2026-09-05 17:29:42 +0530

    Learned Arithmetic Operators in C++

---
+++ Basic/06_ArithmeticOperators.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+    // arithmetic operators = return the result of a specific
+    //                        arithmeic operation (+ - * /)
+
+    // ORDER
+    // parenthesis
+    // multiplication & division
+    // addition & subtraction
+
+    int students = 20;
+
+    // students = students + 1;
+    // students+=1;
+    // stdudents++;
+
+    // students = students - 2;
+    // students-=2;
+    // students--;
+
+    // students = students * 2;
+    // students*=2;
+
+    // students = students /2;
+    // students/=2;
+
+    int remainder = students % 2;
+
+    std::cout << remainder << '\n';
+
+    return 0;
+}
