a5f9d848c626626fc5f94c13b4ac75c6f2f8e63e
Ansh Kuliyal <kuliyalansh89@gmail.com>
2026-09-09 20:42:09 +0530

    Learned User Defined Function in C++

---
+++ Basic/21_UserDefinedFunction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void happyBirthday(std::string name, int age);
+
+int main()
+{
+    // function = a block of reusable code.
+
+    std::string name = "Steven";
+    int age = 18;
+
+    happyBirthday(name, age);
+
+    return 0;
+}
+void happyBirthday(std::string name, int age)
+{
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "Happy Birthday dear " << name << '\n';
+    std::cout << "Happy Birthday to " << name << '\n';
+    std::cout << "You are " << age << " old" << '\n';
+}
