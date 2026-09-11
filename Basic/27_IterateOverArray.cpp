eff34152458dd8f69f8736385298900f69200057
Ansh Kuliyal <kuliyalansh89@gmail.com>
2026-09-11 23:06:55 +0530

    Learned Iternate Over an Array in C++

---
+++ Basic/27_IterateOverArray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::string students[] = {"Spongebob", "Patrik", "Squidward", "Sandy"};
+
+    for (int i = 0; i < sizeof(students) / sizeof(std::string); i++)
+    {
+        std::cout << students[i] << '\n';
+    }
+
+    return 0;
+}
